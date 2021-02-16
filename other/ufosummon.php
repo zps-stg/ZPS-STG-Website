d381aea2a3f149c01f944eb4e253ff09f8b70f8b
zps-stg <77097138+zps-stg@users.noreply.github.com>
2021-02-15 23:54:21 -0800

    Add files via upload

---
+++ other/ufosummon.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ZPS STG</title>
+	</head>
+<body bgcolor="#000000" text="ffa635" link="a5c2ff" alink="981213" vlink="a5c2ff">
+	<center>
+		<h1>
+		<font face="Arial" color="ffffff">ZPS STG</font>
+		</h1>
+		Last Update: 2021 February 16
+		<form>
+			<a target="_top" href="../">Home</a>	| 
+			<a target="_top" href="../other">Back to Other Pages</a>
+		</form>
+	</center>
+	<hr>
+		<font face="Arial" size="+3" color="ffffff">
+			<center><b>UFO Summon Score Calculator</b></center>
+		</font>
+	<hr>
+		</br>
+		<font face="Arial">
+			<label id='score' for='color'><?php echo tl_term('Color') ?></label>
+			<select id='color'>
+				<option id='red' value='Red'><?php echo tl_color('Red') ?></option>
+				<option id='green' value='Green'><?php echo tl_color('Green') ?></option>
+				<option id='blue' value='Blue'><?php echo tl_color('Blue') ?></option>
+				<option id='rainbow' value='Rainbow'><?php echo tl_color('Rainbow') ?></option>
+			</select>
+		</font>
+</body>
+</html>
